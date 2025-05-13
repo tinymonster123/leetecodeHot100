f913efb4effbcad4248d04b828173c55ab5376e0
tinymonster123 <lfhxgs123@gmail.com>
2025-05-13 11:40:33 +0800

    feat: 二叉树的中序遍历

--- 94.二叉树的中序遍历/index.js
+++ 94.二叉树的中序遍历/index.js
@@ -11,5 +11,11 @@
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
-    
+  return root
+    ? [
+        ...inorderTraversal(root.left),
+        root.val,
+        ...inorderTraversal(root.right),
+      ]
+    : [];
 };
